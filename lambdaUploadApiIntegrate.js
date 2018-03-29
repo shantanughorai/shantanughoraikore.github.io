476c417881d087a34f4b6c26c2b4909d2ac10f9e
shantanughorai <shantanu.ghorai@kore.com>
2018-03-29 15:34:43 +0530

    added cred.json
    added cred.json

--- lambdaUploadApiIntegrate.js
+++ lambdaUploadApiIntegrate.js
@@ -1,7 +1,9 @@
 var AWS = require('aws-sdk');
 var parentId,restApiId, newParentId, funcUri;
+var cred = require('./credentials.json');
 AWS.config.region = 'us-east-2';
-AWS.config.credentials.accessKeyId = 'AKIAIQED6OQ3JE2A7NYA';
+AWS.config.credentials.accessKeyId = cred.credentials.aws_access_key_id;
+AWS.config.credentials.secretAccessKey = cred.credentials.aws_secret_access_key;
 var region = 'us-east-2';
 var apig = new AWS.APIGateway({apiVersion: '2015/07/09'/* '2015/07/09' */});
 var lambda = new AWS.Lambda();
